fc71bea32346830be2093957dad3be2662b31c49
kqf <q.dev.null@gmail.com>
2025-02-26 05:38:47 +0000

    Add normal iteration (#2)
    * Ignore other files
    
    * Add the video data loop

--- cvframes/iterate.py
+++ cvframes/iterate.py
@@ -5,13 +5,35 @@ import cv2
 import numpy as np
 
 
+def video_data(
+    ipath: Path,
+    opath: Path | str = "",
+    start_frame: int = -1,
+    stop_frame: int = -1,
+):
+    capture = cv2.VideoCapture(str(ipath))
+    capture.icap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    count = start_frame
+    try:
+        while True:
+            ret, frame = capture.read()
+            count += 1
+            if not ret:
+                break
+            if stop_frame > 0 and count >= stop_frame:
+                break
+            yield capture, frame
+    finally:
+        capture.release()
+
+
 def iterate_sbs(
-    path: Path,
+    ipath: Path,
 ) -> Generator[Tuple[np.ndarray, np.ndarray], None, None]:
-    capture = cv2.VideoCapture(str(path))
+    capture = cv2.VideoCapture(str(ipath))
 
     if not capture.isOpened():
-        print(f"Error: Could not open video {path}")
+        print(f"Error: Could not open video {ipath}")
         return
 
     while True:
