2f7f413056c669361acbf5359809cd8cc9459eff
Sasha <q.dev.null@gmail.com>
2025-03-13 06:46:11 +0100

    Fix hte other tests

--- cvframes/iterate.py
+++ cvframes/iterate.py
@@ -42,7 +42,7 @@ def iterate_generic(
     process_frames: Callable[[np.ndarray], T],
 ) -> Generator[T, None, None]:
     capture = IOCapture(str(ipath), oname=opath or "")
-    capture.icap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    capture.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
     count = start_frame
 
     if not capture.isOpened():
